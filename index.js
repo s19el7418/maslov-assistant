2c2c1a6024c3bbc334f8d2253b8f305b57ee9d70
Nick <120681875+s19el7418@users.noreply.github.com>
2024-04-03 00:19:30 +0300

    1.0.2

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import { Telegraf } from 'telegraf';
 
 console.log('Maslov Assistant server is active!');
 
-const GEMINI_API_KEY = 'AIzaSyD19GL4EEAL2P-OCVRG26BFD-kYKfqdIiU';
-const TELEGRAM_API_KEY = '6829499066:AAHOvIw2dxDuyhLZt13IhN2jF5P9ml-Dcrw';
+const GEMINI_API_KEY = 'YOUR KEY';
+const TELEGRAM_API_KEY = 'YOUR KEY';
 
 const bot = new Telegraf(TELEGRAM_API_KEY);
 const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
